c08713bdbe64316158e5ad6ca15a39e1d2bedcc9
ConeCut <enachescucosmin2016@gmail.com>
2023-11-08 10:33:52 +0100

    Added login page, with error handlers and a system that shows wether you are logged in or not

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'includes/config_session.inc.php';
 require_once 'includes/signup_view.inc.php';
+require_once 'includes/login_view.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -9,16 +10,35 @@ require_once 'includes/signup_view.inc.php';
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/main.css" type="text/css">
     <link rel="stylesheet" href="css/reset.css" type="text/css">
-    <title>Login</title>
+    <title>Welcome!</title>
 </head>
 <body>
 
-<h3>Login</h3>
-<form action="includes/login.inc.php" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="pwd" placeholder="Password">
-    <button>Login</button>
-</form>
+<h3>
+
+    <?php
+
+    output_username();
+
+    ?>
+
+</h3>
+<?php
+
+if (!isset($_SESSION["user_id"])){ ?>
+
+    <h3>Login</h3>
+    <form action="includes/login.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd" placeholder="Password">
+        <button>Login</button>
+    </form>
+
+<?php } ?>
+
+<?php
+check_login_errors();
+?>
 
 <form action="includes/signup.inc.php" method="post">
     <?php
@@ -30,6 +50,11 @@ require_once 'includes/signup_view.inc.php';
 <?php
 check_signup_errors();
 ?>
+
+<h3>Logout</h3>
+<form action="includes/logout.inc.php" method="post" class="logoutForm">
+    <button>Logout</button>
+</form>
 
 </body>
 </html>
